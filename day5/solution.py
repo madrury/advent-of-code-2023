a7f52542fbbf8876859c40a8c770bdc2c8e764a5
Matthew Drury <matthew.drury.83@gmail.com>
2023-12-06 15:30:09 -0800

    Day 5.2. First try. Felt good.

--- day5/solution.py
+++ day5/solution.py
@@ -16,11 +16,18 @@ class Segment:
             return (x - self.source_start) + self.destination_start
         return None
 
+    def eval_range(self, r: range) -> range:
+        assert (r.start >= self.range.start) and (r.stop <= self.range.stop)
+        return range(
+            (r.start - self.source_start) + self.destination_start,
+            (r.stop - self.source_start) + self.destination_start
+        )
+
 @dataclass
 class Map:
     source: str
     target: str
-    segments: [Segment]
+    segments: List[Segment]
 
     def eval(self, x: int) -> int:
         for s in self.segments:
@@ -28,6 +35,41 @@ class Map:
             if maybe: return maybe
         return x
 
+    def eval_range(self, r: range) -> [range]:
+        queue: List[range] = [r]
+        output: List[range] = []
+        for segment in self.segments:
+            nqueue = []
+            for r in queue:
+                nqueue.append(left_difference(r, segment.range))
+                nqueue.append(right_difference(r, segment.range))
+                i = intersect(r, segment.range)
+                if i:
+                    output.append(segment.eval_range(i))
+            queue = [r for r in nqueue if r is not None]
+        return output + queue
+
+
+# -- range API functions ---
+def intersect(r0: range, r1: range) -> range | None:
+    if r0.start > r1.start:
+        r0, r1 = r1, r0
+    if r0.stop <= r1.start:
+        return None
+    return range(max(r0.start, r1.start), min(r0.stop, r1.stop))
+
+def left_difference(r0: range, r1: range) -> range | None:
+    if r0.start < r1.start:
+        return range(r0.start, min(r0.stop, r1.start))
+    return None
+
+def right_difference(r0: range, r1: range) -> range | None:
+    if r0.stop > r1.stop:
+        return range(max(r0.start, r1.stop), r0.stop)
+    return None
+
+
+# --- Parsing ---
 class ParseStates(Enum):
     SEEDS = 0
     BLANK = 1
@@ -68,6 +110,11 @@ def parse_segment_line(line: str) -> Segment:
     rng = range(sstart, sstart + rlen)
     return Segment(sstart, dstart, range=rng)
 
+def to_seed_ranges(seeds: List[Seed]) -> List[range]:
+    return [
+        range(x, x + y) for x, y in zip(seeds[0::2], seeds[1::2])
+    ]
+
 def evaluate_seed(seed: Seed, atlas: Dict[str, Map]) -> int:
     map = atlas['seed']
     target = map.target
@@ -76,6 +123,17 @@ def evaluate_seed(seed: Seed, atlas: Dict[str, Map]) -> int:
         if target == 'location':
             return map.eval(x)
         x = map.eval(x)
+        map = atlas[target]
+        target = map.target
+
+def evaluate_seed_range(seedrange: range, atlas: Dict[str, Map]) -> int:
+    map = atlas['seed']
+    target = map.target
+    xs: List[range] = [seedrange]
+    while target:
+        if target == 'location':
+            return sum([map.eval_range(x) for x in xs], [])
+        xs = sum([map.eval_range(x) for x in xs], [])
         map = atlas[target]
         target = map.target
 
@@ -94,3 +152,11 @@ if __name__ == '__main__':
     for seed in seeds:
         locations.append(evaluate_seed(seed, atlas))
     print(f"The minimum location number is: {min(locations)}")
+
+    location_ranges = []
+    seed_ranges = to_seed_ranges(seeds)
+    for srng in seed_ranges:
+        location_ranges.append(evaluate_seed_range(srng, atlas))
+
+    location_ranges = sum(location_ranges, [])
+    print(f"The minimum location number is: {min(r.start for r in location_ranges)}")
